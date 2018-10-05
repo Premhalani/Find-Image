2eb037975bab73a07555ae031ffd48f3833b9b64
Premhalani <premhalani@gmail.com>
2018-10-05 01:08:58 -0700

    make splash screen more customizable

--- app/src/main/java/com/example/prem/findimage/SplashActivity.java
+++ app/src/main/java/com/example/prem/findimage/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.prem.findimage;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -16,13 +17,15 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.splash_screen);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
